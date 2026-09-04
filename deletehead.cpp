2557b61bcd56c36be12cd9b4a9ca4546c66fc540
Subhankar Biswal <subhankarbiswal57@gmail.com>
2026-09-05 03:52:31 +0530

    Add linked list delete head operation

---
+++ deletehead.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+Node *traverse(vector<int>arr)
+{
+    Node *head=new Node(arr[0]);
+    Node *trav=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node *temp=new Node(arr[i]);
+        trav->next=temp;
+        trav=trav->next;
+    }
+    return head;
+}
+Node *DeleteHead(Node *head)
+{
+    Node *temp=head;
+    if(temp==NULL)
+    return NULL;
+    head=head->next;
+    delete temp;
+    return head;
+}
+void print(Node *head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int>arr={1,2,3,4,5,6,7,8,9};
+    Node *head=traverse(arr);
+    head=DeleteHead(head);
+    print(head);
+}
