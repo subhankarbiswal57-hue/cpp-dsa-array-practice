bb5a2fc2892792a867b05523cabd53b653e53b79
Subhankar Biswal <subhankarbiswal57@gmail.com>
2026-09-16 23:55:24 +0530

    Add second largest element demo program

---
+++ leftrotateby1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int temp = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+
+    arr[n - 1] = temp;
+
+    cout << "Array after left rotation by 1: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
